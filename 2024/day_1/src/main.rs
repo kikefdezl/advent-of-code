cf229930dee9ef7de5d18cca0211c7a213053586
kike <enriquelagui@outlook.com>
2024-12-02 08:38:52 +0100

    Move parts into separate functions

--- 2024/day_1/src/main.rs
+++ 2024/day_1/src/main.rs
@@ -2,6 +2,35 @@ use std::fs;
 
 const INPUT_FILE: &str = "input.txt";
 
+fn part_one(left_list: &Vec<i32>, right_list: &Vec<i32>) {
+    let mut total_distance: i32 = 0;
+    for (l, r) in left_list.iter().zip(right_list.iter()) {
+        let distance = l - r;
+        total_distance += distance.abs();
+    }
+    println!("Total distance: {}", total_distance);
+}
+
+fn part_two(left_list: &Vec<i32>, right_list: &Vec<i32>) {
+    let right_list_len = right_list.len();
+    let mut total_similarity_score: i32 = 0;
+    let mut r_idx: usize = 0;
+    for l in left_list {
+        while r_idx < right_list_len && &right_list[r_idx] < l {
+            r_idx += 1;
+        }
+
+        let mut n_appearances = 0;
+        while r_idx < right_list_len && &right_list[r_idx] == l {
+            n_appearances += 1;
+            r_idx += 1;
+        }
+        total_similarity_score += l * n_appearances;
+    }
+
+    println!("Total similarity score: {}", total_similarity_score);
+}
+
 fn main() {
     let contents = fs::read_to_string(INPUT_FILE).unwrap();
 
@@ -17,28 +46,6 @@ fn main() {
     left_list.sort();
     right_list.sort();
 
-    let mut total_distance: i32 = 0;
-    for (l, r) in left_list.iter().zip(right_list.iter()) {
-        let distance = l - r;
-        total_distance += distance.abs();
-    }
-    println!("Total distance: {}", total_distance);
-
-    let right_list_len = right_list.len();
-    let mut total_similarity_score: i32 = 0;
-    let mut r_idx: usize = 0;
-    for l in &left_list {
-        while r_idx < right_list_len && &right_list[r_idx] < l {
-            r_idx += 1;
-        }
-
-        let mut n_appearances = 0;
-        while r_idx < right_list_len && &right_list[r_idx] == l {
-            n_appearances += 1;
-            r_idx += 1;
-        }
-        total_similarity_score += l * n_appearances;
-    }
-
-    println!("Total similarity score: {}", total_similarity_score);
+    part_one(&left_list, &right_list);
+    part_two(&left_list, &right_list);
 }
